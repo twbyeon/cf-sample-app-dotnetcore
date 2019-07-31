ab9286e7d2a8b4171efe39fe89291c88ab5c4270
twbyeon <39757981+twbyeon@users.noreply.github.com>
2019-07-31 10:37:57 +0900

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 
 namespace CfSampleAppDotNetCore
 {
-    private class Program
+    public class Program
     {
         public static void Main(string[] args)
         {
